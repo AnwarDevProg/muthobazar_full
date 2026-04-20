18d254ae105a6fb58828d255fb19ad52a1a01866
Md Anwar Hossain <ahsourav@gmail.com>
2026-04-20 15:42:45 +0600

    Working with price resolver and customer app home page

--- packages/shared_repositories/lib/home/datasources/mb_home_local_data_source.dart
+++ packages/shared_repositories/lib/home/datasources/mb_home_local_data_source.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hive/hive.dart';
 import 'package:shared_models/home/mb_home_cache_bundle.dart';
 
@@ -18,18 +19,17 @@ class MBHiveHomeLocalDataSource implements MBHomeLocalDataSource {
   static const String _boxName = 'mb_home_cache_box';
   static const String _bundleKey = 'home_bundle';
 
-  Future<Box<String>> _openBox() async {
+  Future<Box<dynamic>> _openBox() async {
     if (Hive.isBoxOpen(_boxName)) {
-      return Hive.box<String>(_boxName);
+      return Hive.box<dynamic>(_boxName);
     }
-
-    return Hive.openBox<String>(_boxName);
+    return Hive.openBox<dynamic>(_boxName);
   }
 
   @override
   Future<MBHomeCacheBundle?> readHomeBundle() async {
-    final Box<String> box = await _openBox();
-    final String? rawJson = box.get(_bundleKey);
+    final Box<dynamic> box = await _openBox();
+    final String? rawJson = box.get(_bundleKey) as String?;
 
     if (rawJson == null || rawJson.isEmpty) {
       return null;
@@ -37,7 +37,6 @@ class MBHiveHomeLocalDataSource implements MBHomeLocalDataSource {
 
     try {
       final Object decoded = json.decode(rawJson);
-
       if (decoded is! Map) {
         await box.delete(_bundleKey);
         return null;
@@ -54,14 +53,56 @@ class MBHiveHomeLocalDataSource implements MBHomeLocalDataSource {
 
   @override
   Future<void> saveHomeBundle(MBHomeCacheBundle bundle) async {
-    final Box<String> box = await _openBox();
-    final String rawJson = json.encode(bundle.toMap());
+    final Box<dynamic> box = await _openBox();
+
+    final Map<String, dynamic> rawMap =
+    Map<String, dynamic>.from(bundle.toMap());
+
+    final Object? safeMap = _jsonSafe(rawMap);
+    final String rawJson = json.encode(safeMap);
+
     await box.put(_bundleKey, rawJson);
   }
 
   @override
   Future<void> clearHomeBundle() async {
-    final Box<String> box = await _openBox();
+    final Box<dynamic> box = await _openBox();
     await box.delete(_bundleKey);
+  }
+
+  Object? _jsonSafe(Object? value) {
+    if (value == null ||
+        value is String ||
+        value is num ||
+        value is bool) {
+      return value;
+    }
+
+    if (value is DateTime) {
+      return value.toIso8601String();
+    }
+
+    if (value is Timestamp) {
+      return value.toDate().toIso8601String();
+    }
+
+    if (value is Iterable) {
+      return value.map(_jsonSafe).toList(growable: false);
+    }
+
+    if (value is Map) {
+      return value.map(
+            (key, val) => MapEntry(
+          key.toString(),
+          _jsonSafe(val),
+        ),
+      );
+    }
+
+    if (value is Enum) {
+      return value.name;
+    }
+
+    return value.toString();
   }
 }
